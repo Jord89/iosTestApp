db1cb26b7dd80e9961ae54d960f7b399a1d2fe56
Jordan <j.homan@me.com>
2012-08-22 08:58:45 -0400

    This is my first commit test.

---
+++ Test App/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test App
+//
+//  Created by Jordan Homan on 8/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test AppTests/Test_AppTests.h
@@ -0,0 +1,13 @@
+//
+//  Test_AppTests.h
+//  Test AppTests
+//
+//  Created by Jordan Homan on 8/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Test_AppTests : SenTestCase
+
+@end
